d37228e0d1253ce065fa9d7e38bcfcc1d488f1ff
vivekrupadhyay <viveku407@gmail.com>
2020-09-14 22:18:34 +0530

    implement toaster

---
+++ client/src/app/Shared/Common/services/toaster.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { ToastrNotification } from '../toastrnotification';
+import { Router, NavigationStart } from '@angular/router';
+import { NotificationType } from '../notification-type.enum';
+
+@Injectable()
+export class ToasterService {
+  public subject = new Subject<ToastrNotification>();
+  public keepAfterRouteChange = true;
+  constructor(public router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        if (this.keepAfterRouteChange) {
+          // only keep for a single route change
+          this.keepAfterRouteChange = false;
+        } else {
+          // clear alert messages
+          this.clear();
+        }
+      }
+    });
+  }
+  getAlert(): Observable<any> {
+    return this.subject.asObservable();
+  }
+
+  success = (message: string, keepAfterRouteChange = false): void => {
+    this.showNotification(
+      NotificationType.Success,
+      message,
+      keepAfterRouteChange
+    );
+  };
+
+  error = (message: string, keepAfterRouteChange = false): void => {
+    this.showNotification(
+      NotificationType.Error,
+      message,
+      keepAfterRouteChange
+    );
+  };
+
+  info = (message: string, keepAfterRouteChange = false): void => {
+    this.showNotification(NotificationType.Info, message, keepAfterRouteChange);
+  };
+
+  warn = (message: string, keepAfterRouteChange = false): void => {
+    this.showNotification(
+      NotificationType.Warning,
+      message,
+      keepAfterRouteChange
+    );
+  };
+
+  showNotification = (
+    type: NotificationType,
+    message: string,
+    keepAfterRouteChange = false
+  ): void => {
+    this.keepAfterRouteChange = keepAfterRouteChange;
+    this.subject.next({ type, message } as ToastrNotification);
+  };
+
+  clear(): void {
+    this.subject.next();
+  }
+}
--- client/src/app/Shared/toster/toster.component.ts
+++ client/src/app/Shared/toster/toster.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrNotification } from '../Common/toastrnotification';
+import { NotificationType } from '../Common/notification-type.enum';
+import { ToasterService } from '../Common/services/toaster.service';
 
 @Component({
   selector: 'app-toster',
   templateUrl: './toster.component.html',
-  styleUrls: ['./toster.component.scss']
+  styleUrls: ['./toster.component.scss'],
 })
 export class TosterComponent implements OnInit {
-
-  constructor() { }
+  notifications: ToastrNotification[] = [];
+  constructor(public notificationService: ToasterService) {}
 
   ngOnInit(): void {
+    this.notificationService
+      .getAlert()
+      .subscribe((alert: ToastrNotification) => {
+        this.notifications = [];
+        if (!alert) {
+          this.notifications = [];
+          return;
+        }
+        this.notifications.push(alert);
+        setTimeout(() => {
+          this.notifications = this.notifications.filter((x) => x !== alert);
+        }, 8000);
+      });
   }
-
+  removeNotification = (notification: ToastrNotification): void => {
+    this.notifications = this.notifications.filter((x) => x !== notification);
+  };
+  cssClass = (notification: ToastrNotification) => {
+    if (!notification) {
+      return;
+    }
+    switch (notification.type) {
+      case NotificationType.Success:
+        return 'toast-success';
+      case NotificationType.Error:
+        return 'toast-error';
+      case NotificationType.Info:
+        return 'toast-info';
+      case NotificationType.Warning:
+        return 'toast-warning';
+    }
+  };
 }
